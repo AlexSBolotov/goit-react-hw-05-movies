984248db03c9e976d8a09aacd3c656628973be26
AlexSBolotov <122620169+AlexSBolotov@users.noreply.github.com>
2023-05-08 15:01:35 +0300

    Update SharedLayout.jsx

--- src/components/SharedLayout/SharedLayout.jsx
+++ src/components/SharedLayout/SharedLayout.jsx
@@ -1,6 +1,6 @@
 import { Outlet, NavLink } from 'react-router-dom';
 import { Suspense } from 'react';
-import clsx from 'clsx';
+// import clsx from 'clsx';
 
 import s from './SharedLayout.module.css';
 
